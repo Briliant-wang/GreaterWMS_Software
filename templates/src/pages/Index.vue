f586265929c62b1abe52fe6e47f3775920c0c443
Briliant Wang <2438121546@qq.com>
2021-10-13 11:15:44 +0800

    INIT

---
+++ templates/src/pages/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page class="flex flex-center">
+      <lottie-web-cimo ref="lottie_web" style="width: 40%; max-width: 70%"/>
+  </q-page>
+</template>
+<script>
+import LottieWebCimo from 'components/lottie-web-cimo'
+import { database } from '../db/database'
+import { LocalStorage } from 'quasar'
+
+export default {
+  name: 'PageIndex',
+  components: { LottieWebCimo },
+  data () {
+    return {
+      cleardata: []
+    }
+  },
+  mounted: function () {
+    var page = database.getInstance().get().test
+    page.toArray().then(res => {
+      if (res.length > 0) {
+        this.cleardata = []
+        page.each(result => {
+          this.cleardata.push(result.id)
+        })
+        page.bulkDelete(this.cleardata)
+        this.cleardata = []
+      } else {
+        page.add({
+          id: 1,
+          test: 'next'
+        })
+      }
+    })
+  },
+  watch () {
+  }
+}
+</script>
---
+++ templates/src/pages/uploadcenter/upload.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex flex-top">
+    <template>
+    <div class="q-pa-md">
+      <div class="q-gutter-y-md" style="max-width: 100%">
+        <q-tabs
+          v-model="detaillink"
+        >
+          <transition appear enter-active-class="animated zoomIn">
+            <q-route-tab v-show="$q.localStorage.getItem('staff_type') !== 'Supplier' &&
+                          $q.localStorage.getItem('staff_type') !== 'Customer'
+                         " name="initializeupload" :label="$t('upload_center.initializeupload')" icon="img:statics/uploadcenter/uploadinbound.png" to="/uploadcenter/initializeupload" exact/>
+          </transition>
+        </q-tabs>
+      </div>
+    </div>
+  </template>
+    <template>
+      <div class="q-pa-md">
+        <div class="q-gutter-y-md" style="max-width: 100%">
+          <q-tabs
+            v-model="detaillink"
+          >
+            <transition appear enter-active-class="animated zoomIn">
+              <q-route-tab v-show="$q.localStorage.getItem('staff_type') !== 'Supplier' &&
+                          $q.localStorage.getItem('staff_type') !== 'Customer'
+                         " name="addupload" :label="$t('upload_center.addupload')" icon="img:statics/uploadcenter/addupload.png" to="/uploadcenter/addupload" exact/>
+            </transition>
+          </q-tabs>
+        </div>
+      </div>
+    </template>
+    <div class="main-table">
+      <router-view />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'upload'
+}
+</script>
+
+<style scoped>
+
+</style>
